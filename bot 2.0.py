648ad5da878c4b23a7590ca5b06cb787f8106395
CadejoBlanko <132834938+CadejoBlanko@users.noreply.github.com>
2023-06-11 12:38:52 +0300

    Update and rename bot.py to bot 2.0.py

--- bot 2.0.py
+++ bot 2.0.py
@@ -1,6 +1,40 @@
+from collections import UserDict
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    pass
+
+
+class Record:
+    def __init__(self, name):
+        self.name = name
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(phone)
+
+    def remove_phones(self):
+        self.phones = []
+
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self[record.name.value] = record
+
+
 def normalization_text(user_text):
     lower_user_text = user_text.lower()
     return lower_user_text
+
 
 def input_error(user_text, normalization_user_text):
     user_text_split = user_text.split(" ")
@@ -10,7 +44,7 @@ def input_error(user_text, normalization_user_text):
     if not normalization_user_text.startswith(tuple_cmd):
         print(f"Wrong command {normalization_user_text_split[0]}. Please try again.")
         return False
-    
+
     if normalization_user_text.startswith(("add", "change")):
         if not len(user_text_split) >= 3:
             print("Give me name and phone please")
@@ -26,53 +60,85 @@ def input_error(user_text, normalization_user_text):
                 print("Enter phone")
                 return False
         elif not user_text_split[2].isdigit():
-                print("Enter phone")
-                return False  
-        
+            print("Enter phone")
+            return False
+
     if normalization_user_text.startswith("phone"):
         if not user_text_split[1].isalpha():
             print("Enter user name")
             return False
     return True
 
+
 def cmd_hello():
     print("How can I help you?")
 
-contact_dict = {}
+
+address_book = AddressBook()
+
 
 def cmd_add(text):
     text_split = text.split(" ")
-    if text_split[1] not in contact_dict:  
-        contact_dict[text_split[1]] = text_split[2]
-    #print(contact_dict)
+    name = Name(text_split[1])
+    phone = Phone(text_split[2])
+
+    if name.value not in address_book:
+        record = Record(name)
+        record.add_phone(phone)
+        address_book.add_record(record)
+        print("Contact added successfully.")
+    else:
+        print("Contact already exists.")
+
 
 def cmd_change(text):
     text_split = text.split(" ")
-    if text_split[1] in contact_dict:  
-        contact_dict[text_split[1]] = text_split[2]
-    #print(contact_dict)
+    name = Name(text_split[1])
+    phone = Phone(text_split[2])
+
+    if name.value in address_book:
+        record = address_book[name.value]
+        record.remove_phones()
+        record.add_phone(phone)
+        print("Contact updated successfully.")
+    else:
+        print("Contact not found.")
+
 
 def cmd_phone(text):
     text_split = text.split(" ")
-    name = text_split[1]
-    print(contact_dict[name])
+    name = Name(text_split[1])
+
+    if name.value in address_book:
+        record = address_book[name.value]
+        if record.phones:
+            for phone in record.phones:
+                print(phone.value)
+        else:
+            print("No phone numbers found for this contact.")
+    else:
+        print("Contact not found.")
+
 
 def cmd_show_all():
-    sorted_contact_dict = {key: contact_dict[key] for key in sorted(contact_dict.keys())}
-    for contact, phone in sorted_contact_dict.items():
-        print(contact, phone)
+    if address_book:
+        for name, record in sorted(address_book.items()):
+            print(f"{name}:")
+            if record.phones:
+                for phone in record.phones:
+                    print(phone.value)
+            else:
+                print("No phone numbers found for this contact.")
+    else:
+        print("Address book is empty.")
 
 
 def main():
-
     work = True
     while work:
-
         user_text = input()
         normalization_user_text = normalization_text(user_text)
-
         if input_error(user_text, normalization_user_text):
-
             if normalization_user_text.startswith("hello"):
                 cmd_hello()
             elif normalization_user_text.startswith("add"):
@@ -86,5 +152,5 @@ def main():
             elif normalization_user_text.startswith(("exit", "good bye", "close")):
                 work = False
 
-
+    
 main()
